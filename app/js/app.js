3803e558a91815c19f765e259af441b201390671
Rodion <rodion18570@gmail.com>
2020-08-11 16:46:52 +0300

    created sliders for main page

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(()=>{
 
-	// Custom JS
+	let owlBestsellers = $('.bestsellers .row'),
+		owlManufacturers = $('.manufacturers .row');
 
+	// Слайдер хитов продаж
+	$('.bestsellers .row').owlCarousel({
+		items:1.3,
+		loop:true,
+		autoplay:true,
+		autoplayTimeout:3000,
+		responsive:{
+			1200:{
+				items:4,
+			}
+		}
+
+	});
+
+	$('.bestsellers__arrow.arrow_left').click(function() {
+		owlBestsellers.trigger('prev.owl.carousel');
+	});
+	$('.bestsellers__arrow.arrow_right').click(function() {
+		owlBestsellers.trigger('next.owl.carousel');
+	});
+
+	// Слайдер производителей
+	$('.manufacturers .row').owlCarousel({
+		items:1.3,
+		loop:true,
+		autoplay:true,
+		autoplayTimeout:3500,
+		responsive:{
+			1200:{
+				items:6,
+			}
+		}
+	});
+
+	$('.manufacturers__arrow.arrow_left').click(function() {
+		owlManufacturers.trigger('prev.owl.carousel');
+	});
+	$('.manufacturers__arrow.arrow_right').click(function() {
+		owlManufacturers.trigger('next.owl.carousel');
+	});
 });
